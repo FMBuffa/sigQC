5a2024286ce4bd7cdfcf756f61c74358966f81e3
alexb-84 <alessandro.barberis01@gmail.com>
2017-04-29 14:18:55 +0100

    added a functioning example

--- R/make_all_plots.R
+++ R/make_all_plots.R
@@ -13,14 +13,19 @@
 #' @keywords make_all_plots
 #' @export
 #' @examples
-# ' names = c("dataset1")
-# ' data.matrix = replicate(10, rnorm(20))#random matrix - 10 genes x 20 samples
-# ' mRNA_expr_matrix = list()
-# ' mRNA_expr_matrix["dataset1"] = list(data.matrix)
-# ' row.names(mRNA_expr_matrix$dataset1) <- as.character(1:(dim(mRNA_expr_matrix$dataset1)[1]))
-# ' colnames(mRNA_expr_matrix$dataset1) <- as.character(1:(dim(mRNA_expr_matrix$dataset1)[2]))
-# ' gene_sig = c('1', '4', '5')#gene ids
-# ' make_all_plots(gene_sig, mRNA_expr_matrix, names)
+#' names = c("dataset1")
+#' data.matrix = replicate(10, rnorm(20))#random matrix - 10 genes x 20 samples
+#' mRNA_expr_matrix = list()
+#' mRNA_expr_matrix[["dataset1"]] = data.matrix
+#' row.names(mRNA_expr_matrix$dataset1) <- as.character(1:(dim(mRNA_expr_matrix$dataset1)[1]))
+#' colnames(mRNA_expr_matrix$dataset1) <- as.character(1:(dim(mRNA_expr_matrix$dataset1)[2]))
+#' #Define the signature
+#' gene_sigs_list = list()
+#' signature = "hypoxiaSig"
+#' gene_sig = c('1', '4', '5')#gene ids
+#' gene_sigs_list[[signature]] = as.matrix(gene_sig)
+#' names_sigs = c(signature)
+#' make_all_plots(names_datasets = names, names_sigs = names_sigs, gene_sigs_list = gene_sigs_list, mRNA_expr_matrix = mRNA_expr_matrix, out_dir = outputdir.path)
 
 
 
